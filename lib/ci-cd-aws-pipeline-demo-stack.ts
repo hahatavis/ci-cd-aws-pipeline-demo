c6061751f6d62b29c1f06077dfd86afa5f8f3520
hahatavis <tavis.ha@gmail.com>
2024-06-02 22:51:52 -0500

    Update ci-cd-aws-pipeline-demo-stack.ts

--- lib/ci-cd-aws-pipeline-demo-stack.ts
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -2,13 +2,13 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep, Step } from 'aws-cdk-lib/pipelines';
 import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
-//import { MyPipelineAppStage } from './stage';
+import { MyPipelineAppStage } from './stage';
 
 export class CiCdAwsPipelineDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'Pipeline', {
+    const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('hahatavis/ci-cd-aws-pipeline-demo', 'main'), //Remember to change 

d2e9396329ad9e9f0b23f26810b213b928c570d6
hahatavis <tavis.ha@gmail.com>
2024-06-03 00:19:25 -0500

    sdfsdfsf

--- lib/ci-cd-aws-pipeline-demo-stack.ts
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -8,7 +8,7 @@ export class CiCdAwsPipelineDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'Pipeline', {
+    const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('hahatavis/ci-cd-aws-pipeline-demo', 'main'), //Remember to change 
@@ -16,6 +16,20 @@ export class CiCdAwsPipelineDemoStack extends cdk.Stack {
                    'npm run build', 
                    'npx cdk synth']
       })
-    })
+    });
+
+
+
+    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: "255628875493", region: "us-east-1" }
+    }));
+
+
+    testingStage.addPre(new ShellStep("Run Unit Tests", { commands: ['npm install', 'npm test'] }));
+    testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
+
+    const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
+      env: { account: "255628875493", region: "us-east-1" }
+    }));
   }
-};
+}
